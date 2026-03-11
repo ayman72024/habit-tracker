94efc4c196f9dc47dbf18f220bd17fdd2bf69262
ayman72024 <ahnafayman11@gmail.com>
2026-03-11 21:55:51 +0600

    defined login

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(_name_)
 def index():
   return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if request.method == "POST":
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    print(username, password)
+    return "Login submitted!"
+return render_template("login.html")
+
 if __name__ == __main__:
   app.run(debug=True)
